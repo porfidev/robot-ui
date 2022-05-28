b7bd9a18a8b9c056ea0eaa7af885eacf7837fa96
Porfirio Chávez <hola@porfi.dev>
2022-05-28 13:47:02 -0500

    update: controls

--- src/components/RobotControls.js
+++ src/components/RobotControls.js
@@ -1,6 +1,5 @@
-import { useEffect, useRef, useState } from 'react';
-import React from 'react';
-import { Circle, Group, Image, Layer, Stage } from 'react-konva';
+import React, { useEffect, useRef, useState } from 'react';
+import { Circle, Group, Layer, Stage } from 'react-konva';
 import { useWindowSize } from 'react-use';
 import styled from 'styled-components';
 
@@ -15,35 +14,69 @@ const RobotControls = ({socket}) => {
   const {width, height} = useWindowSize();
   const yAxisControl = useRef();
   const xAxisControl = useRef();
+  const [isMovingY, setIsMovingY] = useState(false);
+  const [isMovingX, setIsMovingX] = useState(false);
+
   const LimitMovementThreshold = 60;
+  const stickSize = 160;
 
-  const xAxisOriginalPosition = {x: width * 0.26 - (200 / 2), y: height * 0.96 - (200 / 2)};
-  const yAxisOriginalPositon = {x: width * 0.96 - (200 / 2), y: height * 0.96 - (200 / 2)};
+  // const xAxisOriginalPosition = {x: (width * 0.2) - (200 / 2), y: (height * 0.96) - (200 / 2)};
+  const xAxisOriginalPosition = {x: stickSize + (width * 0.05) - (stickSize / 2), y: (height * 0.92) - (stickSize / 2)};
+  const yAxisOriginalPositon = {x: (width * 0.95) - (stickSize / 2), y: (height * 0.92) - (stickSize / 2)};
 
-  const goAhead = (movement) => {
+  useEffect(() => {
+    let testInterval = null;
+    if (isMovingY === true) {
+      testInterval = setInterval(() => {
+        console.log('current Y postion', yAxisControl.current.y());
+        console.log('Movement', evaluateYAxisMovement(yAxisOriginalPositon.y, yAxisControl.current.y()));
+        moveLinearX(evaluateYAxisMovement(yAxisOriginalPositon.y, yAxisControl.current.y()));
+      }, 300);
+    } else {
+      clearInterval(testInterval);
+    }
+
+    return () => {
+      clearInterval(testInterval);
+    };
+  }, [isMovingY]);
+
+  useEffect(() => {
+    let testInterval = null;
+    if (isMovingX === true) {
+      testInterval = setInterval(() => {
+        console.log('current X postion', xAxisControl.current.x());
+        console.log('Movement', evaluateYAxisMovement(xAxisOriginalPosition.x, xAxisControl.current.x()));
+        moveAngularZ(evaluateYAxisMovement(xAxisOriginalPosition.x, xAxisControl.current.x()));
+      }, 300);
+    } else {
+      clearInterval(testInterval);
+    }
+
+    return () => {
+      clearInterval(testInterval);
+    };
+  }, [isMovingX]);
+
+  const moveLinearX = (movement) => {
     socket.emit('Move', {
-      "linear_x": movement,
-      "angular_z": 0
+      'linear_x': movement,
+      'angular_z': 0
+    });
+  };
+
+  const moveAngularZ = (movement) => {
+    socket.emit('Move', {
+      'linear_x': 0,
+      'angular_z': movement
     });
   };
 
   const stop = () => {
     socket.emit('Move', {
-      "linear_x": 0,
-      "angular_z": 0
+      'linear_x': 0,
+      'angular_z': 0
     });
-  }
-
-  const goBackward = () => {
-    socket.emit('Move', {
-      "linear_x": -0.3,
-      "angular_z": 0
-    });
-  };
-
-  const handleDrag = (event) => {
-    console.log('dragging');
-    // setState({...state, x: event.evt.x, y: 200});
   };
 
   const limitPosition = (positionAxis, original) => {
@@ -72,64 +105,38 @@ const RobotControls = ({socket}) => {
     };
   };
 
-  const dragEndHandler = () => {
-    // setState({...state, x: 50, y: 50});
-    console.log('dragend');
-    xAxisControl.current.position({x: xAxisOriginalPosition.x, y: xAxisOriginalPosition.y});
-    yAxisControl.current.position({x: yAxisOriginalPositon.x, y: yAxisOriginalPositon.y});
-  };
-
   // CONSTOL FUNCITON
+  const evaluateYAxisMovement = (originalPosition, currentPosition) => {
+    const evalPosition = originalPosition - currentPosition;
 
-  const [isMovingY, setIsMovingY] = useState(false);
-
-  const evaluateYAxisMovement = (yPosition) => {
-    const evalPosition = yAxisOriginalPositon.y - yPosition;
-
-    console.log('EVAL PO', evalPosition);
-    if(evalPosition === 0) {
-      return 0;
-    }
-
-    if(evalPosition > 0) {
-      if(evalPosition > 40)
+    if (evalPosition > 0) {
+      if (evalPosition > 40)
         return 0.3;
-      if(evalPosition > 20)
-        return 0.2
-      return 0.1
+      if (evalPosition > 20)
+        return 0.2;
+      return 0.1;
     }
 
-    if(evalPosition < 0) {
-      if(evalPosition < -40)
+    if (evalPosition < 0) {
+      if (evalPosition < -40)
         return -0.3;
-      if(evalPosition < -20)
+      if (evalPosition < -20)
         return -0.2;
-      return -0.1
+      return -0.1;
     }
 
+    // WITHOUT MOVEMENT
     return 0;
   };
-
-  useEffect(() => {
-    let testInterval = null;
-    if (isMovingY === true) {
-      testInterval = setInterval(() => {
-        console.log('current Y postion', yAxisControl.current.y());
-        console.log('Movement', evaluateYAxisMovement(yAxisControl.current.y()));
-        goAhead(evaluateYAxisMovement(yAxisControl.current.y()));
-      }, 300);
-    } else {
-      clearInterval(testInterval);
-    }
-
-    return () => {
-      clearInterval(testInterval);
-    };
-  }, [isMovingY]);
 
   const onDragYAxis = (event) => {
     console.log('drag Y', yAxisControl.current.y());
     setIsMovingY(true);
+  };
+
+  const onDragXAxis = (event) => {
+    console.log('drag X', xAxisControl.current.x());
+    setIsMovingX(true);
   };
 
   const onDragEndYAxis = (event) => {
@@ -139,35 +146,42 @@ const RobotControls = ({socket}) => {
     console.log('on End Drag Y', yAxisControl.current.y());
   };
 
+  const onDragEndXAxis = (event) => {
+    setIsMovingX(false);
+    xAxisControl.current.position({x: xAxisOriginalPosition.x, y: xAxisOriginalPosition.y});
+    stop();
+    console.log('on End Drag x', xAxisControl.current.x());
+  };
+
   return (
     <RobotControlsContainer>
       <Stage width={width} height={height}>
         <Layer>
           <Group name="xAxisControlGroup">
-            <Circle width={150} height={150}
+            <Circle width={stickSize} height={stickSize}
                     fill="rgba(0,0,0,0.5)"
                     stroke="rgba(0,0,0,0.7)"
                     x={xAxisOriginalPosition.x}
                     y={xAxisOriginalPosition.y}/>
-            <Circle width={90} height={90}
-                    fill="green"
+            <Circle width={stickSize * 0.6} height={stickSize * 0.6}
+                    fill="rgba(24,183,242,0.4)"
                     x={xAxisOriginalPosition.x}
                     y={xAxisOriginalPosition.y}
                     draggable={true}
-                    onDragMove={handleDrag}
+                    onDragMove={onDragXAxis}
                     dragBoundFunc={dragBoundFuncXHandler}
-                    onDragEnd={dragEndHandler}
+                    onDragEnd={onDragEndXAxis}
                     ref={xAxisControl}/>
           </Group>
 
           <Group name="yAxisControlGroup">
-            <Circle width={150} height={150}
+            <Circle width={stickSize} height={stickSize}
                     fill="rgba(0,0,0,0.5)"
                     stroke="rgba(0,0,0,0.7)"
                     x={yAxisOriginalPositon.x}
                     y={yAxisOriginalPositon.y}/>
-            <Circle width={90} height={90}
-                    fill="green"
+            <Circle width={stickSize * 0.6} height={stickSize * 0.6}
+                    fill="rgba(239,88,80,0.4)"
                     x={yAxisOriginalPositon.x}
                     y={yAxisOriginalPositon.y}
                     draggable={true}
--- src/views/ManualNavigation.jsx
+++ src/views/ManualNavigation.jsx
@@ -1,13 +1,10 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+
+import React from 'react';
 import { useWindowSize } from 'react-use';
 import styled from 'styled-components';
 import BatteryLifeMeter from '../components/BatteryLifeMeter.js';
 import CameraView from '../components/CameraView.js';
 import RobotControls from '../components/RobotControls.js';
-import { EVENT_MODES } from '../enums/eventModes.js';
-
-const serverUrl = process.env.REACT_APP_SOCKET_BASE_URL;
 
 const NavigationContainer = styled.div`
   position: relative;
@@ -18,68 +15,10 @@ const NavigationContainer = styled.div`
 
 const ManualNavigation = ({socket}) => {
   const {width, height} = useWindowSize();
-  const [cameraImage, setCameraImage] = useState('');
-
-  console.log('socket manual navigation', socket);
-  const requestBattery = async () => {
-    await axios.post(`${serverUrl}ros/battery`, {
-      'mode': 'event'
-    });
-  };
-
-  const stopBattery = async () => {
-    await axios.post(`${serverUrl}ros/battery`, {
-      'mode': 'stop'
-    });
-  };
-
-
-
-  // useEffect(() => {
-  //   socket.emit('Connected', {
-  //     'type': 'navigation'
-  //   }, (answer) => {
-  //     console.log('SOCKET ANSWER', answer);
-  //   });
-  //
-  //   socket.on('Camera', CameraListener);
-  //   socket.on('Battery', BatteryListener);
-  //
-  //   return () => {
-  //     socket.off('Battery', BatteryListener);
-  //     socket.off('Camera', CameraListener);
-  //   };
-  // }, [socket]);
-
-  const BatteryListener = data => {
-    console.log('BATTERY MESSAGE', data);
-  };
-
-
-
-
-
-  const goLeft = () => {
-    socket.emit('Move', {
-      "linear_x": 0,
-      "angular_z": -0.2,
-    });
-  };
-
-  const goRight = () => {
-    socket.emit('Move', {
-      "linear_x": 0,
-      "angular_z": 0.2,
-    });
-  };
   return (
     <NavigationContainer width={width} height={height}>
-      <small>You are running this application in <b>{process.env.NODE_ENV}</b> mode.</small>
-      <small>{process.env.REACT_APP_SOCKET_BASE_URL}</small>
-      <button onClick={requestBattery}>iniciar bateria</button>
-      <button onClick={stopBattery}>detener bateria</button>
-      <button onClick={goLeft}>Izquierda</button>
-      <button onClick={goRight}>Derecha</button>
+      {/*<small>You are running this application in <b>{process.env.NODE_ENV}</b> mode.</small>*/}
+      {/*<small>{process.env.REACT_APP_SOCKET_BASE_URL}</small>*/}
       <BatteryLifeMeter />
       <CameraView socket={socket} />
       <RobotControls socket={socket} />
